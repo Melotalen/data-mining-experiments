6984948cf74f7646e4bca39ea0b121161de0f0b5
Soren Goyal <sorengoyal@gmail.com>
2016-09-25 00:12:15 -0400

    auc and confusion matrix

--- incomeAnalysis.r
+++ incomeAnalysis.r
@@ -2,7 +2,7 @@ source('~/projects/data-mining/hw2/incomeKNN.r')
 
 setwd("/home/soren/projects/data-mining/hw2")
 train_data = read.csv("income_tr.csv")
-test_data = read.csv("income_te.csv")[1:100,]
+test_data = read.csv("income_te.csv")
 test_labels = vector(length = nrow(test_data), mode = 'numeric')
 test_labels[test_data$class == ' <=50K'] = 1
 test_labels[test_data$class == ' >50K'] = 2
@@ -11,23 +11,52 @@ train_labels[train_data$class == ' <=50K'] = 1
 train_labels[train_data$class == ' >50K'] = 2
 test_data = incomeScale(train_data[,1:ncol(train_data)-1], test_data[,1:ncol(test_data)-1])
 train_data = incomeScale(train_data[,1:ncol(train_data)-1], train_data[,1:ncol(train_data)-1])
-pred = incomeKnn(5, train_data, train_labels, test_data, test_labels)
-pred2 = incomeKnn(2, train_data, train_labels, test_data, test_labels)
-pred10 = incomeKnn(10, train_data, train_labels, test_data, test_labels)
-pred20 = incomeKnn(20, train_data, train_labels, test_data, test_labels)
+model = generateIncomeModel(train_data, train_labels, test_data)
+
+
+#Geenrating ROC curves
+pred = incomeKnn(5, model, test_labels)
+pred2 = incomeKnn(2, model, test_labels)
+pred10 = incomeKnn(10, model, test_labels)
+pred20 = incomeKnn(20, model, test_labels)
+pred33 = incomeKnn(33, model, test_labels)
 roc = computeROC(pred)
 roc2 = computeROC(pred2)
 roc10 = computeROC(pred10)
 roc20 = computeROC(pred20)
+roc33 = computeROC(pred33)
 jpeg('rocs.jpg', width=500, height=500)
-plot(roc, type = "l", col = "red")
+classes = c("2", "5", "10", "20", "33")
+plot(roc2, type = "l", col = "green", main = "ROC curves for Income Dataset classifier with k = 2, 5, 10 and 20")
 par(new=TRUE)
-plot(roc2, type = "l", col = "green")
+plot(roc, type = "l", col = "red")
 par(new=TRUE)
 plot(roc10, type = "l", col = "blue")
 par(new=TRUE)
 plot(roc20, type = "l", col = "yellow")
-legend("topleft", classes, cex = 1.3, fill = colors)
+par(new=TRUE)
+plot(roc33, type = "l", col = "cyan")
+legend("bottomright", classes, cex = 1.3, fill = c("green", "red", "blue", "yellow", "cyan"))
 dev.off()
 
-computePerformance(pred10)
+#AUC Curve
+areas = vector(mode = 'numeric', length = 100)
+for(i in 1:100){
+  predk = incomeKnn(i, model, test_labels)
+  rock = computeROC(predk)
+  areas[i] = computeAUC(rock)
+}
+plot(areas, type = "l")
+
+#Optimium Cost computation for k=33
+labels = pred33[,1]
+confidences = sort(pred33[,2])
+thresh = vector(mode = 'numeric', length = nrow(confidences))
+cost = vector(mode = 'numeric', length = nrow(confidences))
+cost_matrix = matrix(c(1,-1,-1,1), nrow = 2, ncol = 2 )
+for(i in 1:nrow(confidences)){
+  thresh[i] = confidences[1] + diff(range(confidences))*i/nrow(confidences)
+  mat = computeConfusionMatrix(labels, confidences, thresh[i])
+  cost[i] = cost_matrix*mat
+}
+plot(thresh, cost)
--- incomeKNN.r
+++ incomeKNN.r
@@ -43,86 +43,86 @@ incomeScale = function(source_data, data) {
   return(output)
 }
 
-getIncomePrediction = function(indices, labels){
+getIncomePrediction = function(labels){
   classCount = c(0,0)
-  for(i in 1:length(indices)){
-    classCount[labels[indices[i]]] = classCount[labels[indices[i]]] + 1
+  for(i in 1:length(labels)){
+    classCount[labels[i]] = classCount[labels[i]] + 1
   }
-  maxindex = 1
-  if(classCount[2] > classCount[1])
-    maxindex = 2
-  
-  pred = c(maxindex, classCount[2]/sum(classCount))
-  names(pred) = c("class", "probability")
-  return(pred)
+  return(classCount[2]/sum(classCount))
 }
 
-incomeKnn = function(k, train_data, train_labels, test_data, test_labels){
-    #Creating Datasets Income_original.csv
-    attr.nominal = c("workclass", "marital_status", "occupation", "relationship", "race", "gender", "native_country")
-    attr.numeric = c("age", "capital_gain", "capital_loss", "hour_per_week")
-    attr.ordinal = c("education_cat")
-    for(i in 1:ncol(test_data))
-      levels(test_data[,i]) = levels(train_data[,i])
-    train_data.nominal = train_data[,attr.nominal]
-    train_data.numeric = train_data[,attr.numeric]
-    train_data.ordinal = data.frame(train_data[,attr.ordinal])
-    test_data.nominal = test_data[, attr.nominal]
-    test_data.numeric = test_data[,attr.numeric]
-    test_data.ordinal = data.frame(test_data[,attr.ordinal])
-    #Creating output dataframe
-    output = data.frame(matrix(nrow = nrow(test_data), ncol = 4))
-    names(output) = c("Transaction_ID", "Actual_Label", "Predicted_Label", "Confidence")
-    
-    #Function for computing nominal dissimilarity
-    nominalDissimilarity = function(val1, val2){
-      distance = 0
-      for(i in 1:length(val1)){
-        if(val1[i] != val2[i])
-          distance = distance + 1
-      }
-      return(distance)
+generateIncomeModel = function(train_data, train_labels, test_data){
+  #Creating Datasets Income_original.csv
+  attr.nominal = c("workclass", "marital_status", "occupation", "relationship", "race", "gender", "native_country")
+  attr.numeric = c("age", "capital_gain", "capital_loss", "hour_per_week")
+  attr.ordinal = c("education_cat")
+  for(i in 1:ncol(test_data))
+    levels(test_data[,i]) = levels(train_data[,i])
+  train_data.nominal = train_data[,attr.nominal]
+  train_data.numeric = train_data[,attr.numeric]
+  train_data.ordinal = data.frame(train_data[,attr.ordinal])
+  test_data.nominal = test_data[, attr.nominal]
+  test_data.numeric = test_data[,attr.numeric]
+  test_data.ordinal = data.frame(test_data[,attr.ordinal])
+  #Creating output dataframe
+  output = matrix(nrow = nrow(test_data), ncol = nrow(train_data))
+  #Function for computing nominal dissimilarity
+  nominalDissimilarity = function(val1, val2){
+    distance = 0
+    for(i in 1:length(val1)){
+      if(val1[i] != val2[i])
+        distance = distance + 1
     }
-   
-    
-    #Actual COmputation begins here
-    for(i in 1:nrow(test_data)){
-      print(paste("test element:",i))
-      distances.nominal = vector(mode = "numeric", length = nrow(train_data))
-      distances.numeric = vector(mode = "numeric", length = nrow(train_data))
-      distances.ordinal = vector(mode = "numeric", length = nrow(train_data))
-      for(j in 1:nrow(train_data)){
-        distances.nominal[j] = nominalDissimilarity(train_data.nominal[j,], test_data.nominal[i,])
-        distances.numeric[j] = sqrt(sum((train_data.numeric[i,] - test_data.numeric[j,])^2))
-        distances.ordinal[j] = sqrt(sum((train_data.ordinal[i,] - test_data.ordinal[j,])^2))
-      }
-      distances.total = (ncol(test_data.nominal)*distances.nominal + ncol(test_data.numeric)*distances.numeric + ncol(test_data.ordinal)*distances.ordinal)/(ncol(test_data.nominal) + ncol(test_data.numeric) + ncol(test_data.ordinal))
-      distances.total.sorted = sort(distances.total, index.return = TRUE)
-      prediction = getIncomePrediction(distances.total.sorted$ix[1:k], train_labels)
-      output[i,1] = i
-      output[i,2] = test_labels[i]
-      output[i,3] = prediction["class"] 
-      output[i,4] = prediction["probability"]
-    }
-    return(output)
+    return(distance)
   }
-generateModel = function()
+  
+  #Actual COmputation begins here
+  n_nominal = ncol(test_data.nominal)
+  n_numeric = ncol(test_data.numeric)
+  n_ordinal = ncol(test_data.ordinal)
+  n_total = n_nominal + n_numeric + n_ordinal
+  for(i in 1:nrow(test_data)){
+    print(paste("test element:",i))
+    distances.nominal = vector(mode = "numeric", length = nrow(train_data))
+    distances.numeric = vector(mode = "numeric", length = nrow(train_data))
+    distances.ordinal = vector(mode = "numeric", length = nrow(train_data))
+    for(j in 1:nrow(train_data)){
+      distances.nominal[j] = nominalDissimilarity(train_data.nominal[j,], test_data.nominal[i,])
+      distances.numeric[j] = sqrt(sum((train_data.numeric[j,] - test_data.numeric[i,])^2))
+      distances.ordinal[j] = sqrt(sum((train_data.ordinal[j,] - test_data.ordinal[i,])^2))
+    }
+    distances.total = (n_nominal*distances.nominal + n_numeric*distances.numeric + n_ordinal*distances.ordinal)/(n_total)
+    distances.total.sorted = sort(distances.total, index.return = TRUE)
+    output[i,] = train_labels[distances.total.sorted$ix]
+  }
+  return(output)
+}
+
+incomeKnn = function(k, model, test_labels){
+  output = data.frame(matrix(nrow = nrow(model), ncol = 2))  
+  names(output) = c("Actual_Label", "Confidence")
+  for(i in 1:nrow(model)){
+    output[i,1] = test_labels[i]
+    output[i,2] = getIncomePrediction(model[i,1:k])
+  }
+  return(output)
+}
 
 computeROC = function(predictions){
   negatives = nrow(predictions[predictions$Actual_Label == 1,])
   positives = nrow(predictions[predictions$Actual_Label == 2,])
-  predictions = predictions[order(predictions$Confidence),]
+  predictions = predictions[order(predictions$Confidence, decreasing = TRUE),]
   roc = data.frame()#matrix(nrow = nrow(predictions), ncol = 2))
-  roc[1,1] = nrow(predictions[predictions$Actual_Label == 1,])
-  roc[1,2] = nrow(predictions[predictions$Actual_Label == 2,])
+  roc[1,1] = 0
+  roc[1,2] = 0
   for(i in 1:nrow(predictions)){
     if(predictions$Actual_Label[i] == 1){
-      roc[i+1,1] = roc[i,1] - 1
+      roc[i+1,1] = roc[i,1] + 1
       roc[i+1,2] = roc[i,2]
       }
     else if(predictions$Actual_Label[i] == 2){
       roc[i+1,1] = roc[i,1] 
-      roc[i+1,2] = roc[i,2] - 1
+      roc[i+1,2] = roc[i,2] + 1
     }
   }
   roc[,1] = roc[,1]/negatives
@@ -144,4 +144,33 @@ computePerformance = function(predictions){
   }
   print(paste("Classification Rate:", correct/nrow(predictions),"   Classification Confidence:", classification_confidence/correct))
   print(paste("Misclassification Rate:", incorrect/nrow(predictions)))
+}
+
+computeAUC = function(curve) {
+   area = 0
+   for(i in 2:nrow(curve)){
+     area = area + curve[i,2]*(curve[i,1] - curve[i-1,1])
+   }
+   return(area)
+}
+#       Predicted
+#Actual TP  FN
+#       FP  TN
+computeConfusionMatrix = function(labels, confidences, threshold){
+  confusion_matrix = matrix(data = c(0,0,0,0), nrow = 2, ncol = 2)
+  for(i in 1:nrow(confidences)){
+    if(confidence <= threshold)
+      pred_label = 1
+    else
+      pred_label = 2
+    if(labels[i] == pred_label && labels[i] == 2) #TP
+      confusion_matrix[1,1] = confusion_matrix[1,1] + 1
+    else if(labels[i] != pred_label && labels[i] == 2)
+      confusion_matrix[1,2] = confusion_matrix[1,2] + 1
+    else if(labels[i] != pred_label && labels[i] == 1)
+      confusion_matrix[2,1] = confusion_matrix[2,1] + 1
+    else if(labels[i] == pred_label && labels[i] == 1)
+      confusion_matrix[2,2] = confusion_matrix[2,2] + 1
+  }
+  return(confusion_matrix)
 }
