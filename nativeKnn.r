440ffab70279f2cc1c5aed85a950f34c783cde06
Soren Goyal <sorengoyal@gmail.com>
2016-10-02 02:15:14 -0400

    almost done

--- nativeKnn.r
+++ nativeKnn.r
@@ -69,6 +69,27 @@ jpeg('income_cost.jpg', width=500, height=500)
 plot(thresh, cost, type = "l", col = 'blue', main = "Accuracy Vs Threshold k = 30", xlab = "Threshold", ylab = "Accuracy")
 dev.off()
 
+labels = pred29[,1]
+confidences = sort(pred29[,2])
+thresh = vector(mode = 'numeric', length = length(confidences))
+precision = vector(mode = 'numeric', length = length(confidences))
+recall = vector(mode = 'numeric', length = length(confidences))
+fmeasure = vector(mode = 'numeric', length = length(confidences))
+cost_matrix = matrix(c(1,0,0,1), nrow = 2, ncol = 2 )
+for(i in 1:length(confidences)){
+  thresh[i] = confidences[1] + diff(range(confidences))*i/length(confidences)
+  mat = computeConfusionMatrix(labels, confidences, thresh[i])
+  precision[i] = mat[1,1]/(mat[1,1] + mat[2,1])
+  recall[i] = mat[1,1]/sum(mat[1,])
+  fmeasure[i] = 2*precision[i]*recall[i]/(precision[i] +  recall[i])
+}
+jpeg('prf.jpg', width=500, height=500)
+plot(thresh, recall, type = "l", col = 'green', main = "Precision, Recall and F-Measure Vs Threshold k = 29", xlab = "Threshold", ylab = "Value")
+lines(thresh, precision, type = "l", col = 'blue', main = "Precision, Recall and F-Measure Vs Threshold k = 29", xlab = "Threshold", ylab = "Value")
+lines(thresh, fmeasure, type = "l", col = 'red', main = "Precision, Recall and F-Measure Vs Threshold k = 29", xlab = "Threshold", ylab = "Value")
+legend("topright", c("Recall", "Precision", "F-Measure"), cex = 1.3, fill = c("green", "blue", "red"))
+dev.off()
+
 normalizePredictions = function(prediction){
   pred = data.frame(matrix(nrow = length(test_labels), ncol = 2))  
   names(pred) = c("Actual_Label", "Confidence")
